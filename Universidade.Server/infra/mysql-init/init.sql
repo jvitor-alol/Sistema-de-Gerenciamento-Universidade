391257015982c2e3c4b40a9dffb027e7e82116d3
Guilherme-Soares05 <zoologui86@gmail.com>
2024-03-03 16:04:51 -0300

    Script SQL
    O script gera 3 tabelas sendo elas Alunos, Funcionários e Universidade cada uma com as suas informações respectivas

--- Universidade.Server/infra/mysql-init/init.sql
+++ Universidade.Server/infra/mysql-init/init.sql
@@ -1,14 +1,42 @@
--- Criação do banco de dados da universidade
-CREATE DATABASE IF NOT EXISTS universidade;
-
-USE universidade;
-
-CREATE TABLE IF NOT EXISTS users_test (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL
+-- Tabela de Alunos
+CREATE TABLE Alunos (
+    Nome VARCHAR(50) NOT NULL,
+    Matricula INT NOT NULL,
+    CPF VARCHAR(11) NOT NULL,
+    DataNascimento DATE NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    Rua VARCHAR(100) NOT NULL,
+    Bairro VARCHAR(50) NOT NULL,
+    Cidade VARCHAR(50) NOT NULL,
+    Estado VARCHAR(2) NOT NULL,
+    Numero INT NOT NULL,
+    Logradouro VARCHAR(50) NOT NULL,
+    PRIMARY KEY (Matricula)
 );
 
-INSERT INTO users_test (username, email) VALUES
-('user1', 'user1@example.com'),
-('user2', 'user2@example.com');
+-- Tabela de Funcionários
+CREATE TABLE Funcionarios (
+    Nome VARCHAR(50) NOT NULL,
+    CPF VARCHAR(11) NOT NULL,
+    Cargo VARCHAR(50) NOT NULL,
+    MateriaEspecializacao VARCHAR(50),
+    CargaHoraria VARCHAR(20),
+    EmailProfissional VARCHAR(50) NOT NULL,
+    PRIMARY KEY (CPF)
+);
+
+-- Tabela de Universidade
+CREATE TABLE Universidade (
+    Nome VARCHAR(100) NOT NULL,
+    CNPJ VARCHAR(14) NOT NULL,
+    Servicos VARCHAR(100),
+    Cursos VARCHAR(100),
+    Materias VARCHAR(100),
+    Rua VARCHAR(100) NOT NULL,
+    Bairro VARCHAR(50) NOT NULL,
+    Cidade VARCHAR(50) NOT NULL,
+    Estado VARCHAR(2) NOT NULL,
+    Numero INT NOT NULL,
+    Logradouro VARCHAR(50) NOT NULL,
+    PRIMARY KEY (CNPJ)
+);
